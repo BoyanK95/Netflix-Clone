503e2daadffd4220b22bc7c058487b90c09f7fc7
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-01-23 20:52:37 +0200

    Created initial image header

---
+++ netflix-clone-app/src/components/main/ImgHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImgHeader = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ImgHeader
--- netflix-clone-app/src/components/main/Main.jsx
+++ netflix-clone-app/src/components/main/Main.jsx
@@ -5,16 +5,20 @@ import requests from '../../request/Requests'
 const Main = () => {
     const [movies, setMovies] = useState([])
 
+    const movie = movies[Math.floor(Math.random() * movies.length)]
+
     useEffect(() => {
         axios.get(requests.requestPopular).then((responce) => {
-            setMovies(responce.data)
+            setMovies(responce.data.results)
         })
     }, [])
-    console.log(movies);
+    console.log(movie);
 
   return (
-    <div>
-      
+    <div className='w-full h-[550px] text-white'>
+      <div className='w-full h-full'>
+        <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+      </div>
     </div>
   )
 }
