000ad0cbf6420a8b10bdae1d1587d195062415f0
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-01-23 19:50:44 +0200

    Create Requests.js

---
+++ netflix-clone-app/src/request/Requests.js
@@ -0,0 +1,7 @@
+export const API_KEY = '089b617253e47ebe55e8aeef2a65814f'
+
+const request = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+    requestLatest: `https://api.themoviedb.org/3/movie/latest?api_key=${API_KEY}&language=en-US&page=1`,
+}
